29bbe31c17d8f3373156de5012f7e32db1321dc9
ProgTechsvg <rodrigohend@outlook.com>
2024-01-19 00:44:14 -0300

    add cors no index

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express";
 
+import cors from "cors";
+
 import UserRoute from "./src/Routes/user.route.js";
 import EmpresaRoute from "./src/Routes/empresa.route.js";
 
@@ -21,6 +23,8 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 ConnectMongoDB();
 app.use(express.json());
