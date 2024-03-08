6a0a8f798bc2c1afe96e091b8832ab443caad62d
thersvg <rodrigohend@outlook.com>
2024-03-08 20:40:58 -0300

    updates

--- index.js
+++ index.js
@@ -26,21 +26,14 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 4000;
-/* let dbName = 'DB_FOR_YOU_ENTREGAS';  */
+let dbName = 'DB_FOR_YOU_ENTREGAS'; 
 
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ limit: '5mb', extended: true }));
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // ou especifique domínios permitidos
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
-
 app.use(cors());
 
-ConnectMongoDB();
+ConnectMongoDB(dbName);
 app.use(express.json());
 
 app.use("/documentation", swaggerRoute);
--- src/Database/db.js
+++ src/Database/db.js
@@ -1,8 +1,6 @@
 import mongoose from "mongoose";
 
-import { MongoClient, ServerApiVersion } from 'mongodb';
-
-/* const ConnectMongoDB = (dbName) => {
+const ConnectMongoDB = (dbName) => {
 
   console.log("Esperando conexão com o Banco de dados");
 
@@ -11,29 +9,5 @@ import { MongoClient, ServerApiVersion } from 'mongodb';
     .then(() => console.log("Conectado ao Banco de Dados Mongo Atlas"))
     .catch((error) => console.log("Erro Banco de dados nao conectado", error));
 };
- */
-
-const uri = 'mongodb+srv://foryouentregas:34CCQ2RXQHFi6F38@cluster0.dz5xnii.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0DB_FOR_YOU_ENTREGAS'
-
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function ConnectMongoDB() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
 
 export default ConnectMongoDB; 
