5df9868cf9a9d7c420414d8170342d3bd9c56b0f
thersvg <rodrigohend@outlook.com>
2024-03-08 20:34:28 -0300

    updates

--- index.js
+++ index.js
@@ -31,6 +31,13 @@ const port = process.env.PORT || 4000;
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ limit: '5mb', extended: true }));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // ou especifique domínios permitidos
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.use(cors());
 
 ConnectMongoDB();
