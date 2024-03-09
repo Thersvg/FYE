1bc182ba843e10e24f68cb0012092051d82f7f23
thersvg <rodrigohend@outlook.com>
2024-03-08 23:48:08 -0300

    updated

--- src/Database/db.js
+++ src/Database/db.js
@@ -1,15 +1,9 @@
 import mongoose from "mongoose";
 
 const ConnectMongoDB = (dbName) => {
-
-  const uri = process.env.MONGOODB_URL + `${dbName}`;
-
-  console.log(uri);
-
   console.log("Esperando conexão com o Banco de dados");
-
   mongoose
-    .connect(uri)
+    .connect(process.env.MONGOODB_URL + `${dbName}`)
     .then(() => console.log("Conectado ao Banco de Dados Mongo Atlas"))
     .catch((error) => console.log("Erro Banco de dados nao conectado", error));
 };
