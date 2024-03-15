45b053d09f35edfbe486287dd25f0870f9c1c7e6
thersvg <rodrigohend@outlook.com>
2024-03-15 19:29:50 -0300

    updated

--- src/Controller/RegisterControllerDevice.js
+++ src/Controller/RegisterControllerDevice.js
@@ -3,12 +3,15 @@ import DeviceTokensService from "../Services/DeviceTokens.service.js"
 const NotificationDeviceToken = async (req, res) =>{
     try{
         const { token } = req.body;
-
+        
         if (!token) {
             res.status(400).send('Token não fornecido.');
         }
 
-        await DeviceTokensService.CreateStorageTokenService(token);
+        const body = {
+            token: token
+        }
+        const response = await DeviceTokensService.CreateStorageTokenService(body);
 
         res.status(200).send('Token registrado com sucesso.');
     }catch (error){
